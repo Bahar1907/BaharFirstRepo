a29124643816f538d6928da4aa6da21a13ba2f8d
Bahar1907 <bahar67301@gmail.com>
2022-07-07 20:11:02 -0400

    This is joe branch test

---
+++ JoeBranch/JoePractice.java
@@ -0,0 +1,8 @@
+package JoeBranch;
+
+public class JoePractice {
+    public static void main(String[] args) {
+        System.out.println("Heyy This is Joe Branch, i Justed checkout out ");
+
+    }
+}
